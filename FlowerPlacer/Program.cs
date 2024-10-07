7a09ce728afb7b97f0a0c055988c5e55691ddaf1
Seenath Gooding <seenath.gooding@teleios-systems.com>
2024-10-07 12:45:03 -0400

    finished flower placer

--- FlowerPlacer/Program.cs
+++ FlowerPlacer/Program.cs
@@ -5,11 +5,39 @@ public class Program
 
     public static void Main(string[] args)
     {
+        FlowerPlacer flowerPlacer = new FlowerPlacer();
+        int[] flowerbed = { 1, 0, 0, 0, 1 };
+        int n = 2;
 
+        bool answer = flowerPlacer.CanPlaceFlowers(flowerbed, n);
+        Console.WriteLine(answer);
     }
 }
 
-public class Flower
+public class FlowerPlacer
 {
-
+    public bool CanPlaceFlowers(int[] flowerBed, int n)
+    {
+        bool result = false;
+        int calc = 0;
+        for (int index = 0; index < flowerBed.Length; index++)
+        {
+            if (index + 1 < flowerBed.Length)
+            {
+                if ( (flowerBed[index] ==0) && (index == 0 || flowerBed[index - 1] == 0) && (index == flowerBed.Length-1 || flowerBed[index + 1] == 0) )
+                {
+                    calc++;
+                }
+                //if (flowerBed[index] == 0 && (index ==0 ||flowerBed[index - 1] ==0) && (index == flowerBed.Length - 1) || flowerBed[index + 1] ==0)
+                //{
+                //    
+                //}
+            }
+            if (calc == n)
+            {
+                return result = true;
+            }
+        }
+        return result;
+    }
 }
